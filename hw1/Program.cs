f74e683efa268e73c3d7e41c6ded2900fe4ff36f
VinokurovValeriy <vinokurovvaleriy98@gmail.com>
2019-09-26 17:19:07 +0300

    first commit

---
+++ hw1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int result = 0;
+            int count = 1;
+
+            Console.WriteLine("Write text ");
+            text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+
+                if (text[i] == text[i + 1])
+                {
+                    count++;
+                    if (result < count)
+                    {
+                        result = count;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
+
