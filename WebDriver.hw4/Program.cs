01a2e3c4e224787045bc131fce2b5f76dde513b5
VinokurovValeriy <vinokurovvaleriy98@gmail.com>
2019-11-25 05:03:41 +0300

    dev-4

---
+++ WebDriver.hw4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverTest.hw4
+{
+    class UnreadMessages //displays the number of unread messages
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait waitTime = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://mail.ru");
+            driver.FindElement(By.Id("mailbox:login")).SendKeys("TestWebDriverForDev4" + Keys.Enter);
+
+            IWebElement password = waitTime.Until(ExpectedConditions.ElementIsVisible(By.Id("mailbox:password"))); 
+            password.SendKeys("Homework4" + Keys.Enter); 
+
+            IWebElement unread_messages = waitTime.Until(ExpectedConditions.ElementIsVisible(By.Id("g_mail_events")));
+            Console.Write("The number of unread messages: " + unread_messages.Text + "\n");
+
+            driver.Quit();
+
+        }
+    }
+}
