a917f1ed066ae55956ff2785115ae6501a72484d
VinokurovValeriy <vinokurovvaleriy98@gmail.com>
2019-12-02 23:48:34 +0300

    Unit_tests_hw-2

---
+++ dev-2/UnitTest.hw2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnitTest.hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringAnalizer.MaxCountOfDifferentSymbols("weretryфвмфвм"));
+            Console.WriteLine(StringAnalizer.MaxCountOfIdenticalDigits("222244wbfdeeeeeeeeee32"));
+            Console.WriteLine(StringAnalizer.MaxCountOfIdenticalLatinSymbols("yvbuhninnnnn"));
+        }
+    }
+}
---
+++ dev-2/UnitTest.hw2/StringAnalizer.cs
@@ -0,0 +1,115 @@
+﻿
+
+namespace UnitTest.hw2
+{
+    public class StringAnalizer
+    {
+        public static int MaxCountOfDifferentSymbols (string str)
+        {
+            int count = 1;
+            int maxCount = 1;
+
+            if (str.Length == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i]!=str[i-1])
+                {
+                    count++;
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+
+            }
+            return maxCount;
+        }  
+
+        public static int MaxCountOfIdenticalDigits (string str)
+        {
+            int count = 0;
+            int maxCount = 0;
+
+            foreach (char element in str)
+            {
+               if (element >= 48 && element <= 57)
+                {
+                    count = 1;
+                    maxCount = 1;
+                    break;
+                }
+            }
+
+            if (maxCount==0)
+            {
+                return 0;
+            }
+
+            for (int i = 1; i < str.Length; i++)
+            { 
+                if ((str[i] == str[i - 1])&&(str[i] >= 48 && str[i] <= 57))
+                {
+                    count++;
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            return maxCount;
+        }
+
+        public static int MaxCountOfIdenticalLatinSymbols (string str)
+        {
+            int count = 0;
+            int maxCount = 0;
+
+            foreach (char element in str)
+            {
+                if ((element >= 65 && element <= 90) || (element >= 97 && element <= 122))
+                {
+                    count = 1;
+                    maxCount = 1;
+                    break;
+                }
+            }
+
+            if (maxCount == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if ((str[i] == str[i - 1]) && ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122)))
+                {
+                    count++;
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            return maxCount;
+        }
+
+    }
+}
