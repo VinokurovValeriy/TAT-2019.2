8693a522c2e36c0f9bd2fba9f9f17d72b4994249
VinokurovValeriy <vinokurovvaleriy98@gmail.com>
2019-12-16 18:01:13 +0300

    string_translate

---
+++ DEV-3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace DEV_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transliterator c = new Transliterator();
+            Console.WriteLine(c.GetTranslitString(""));
+        }
+    }
+}
+        
